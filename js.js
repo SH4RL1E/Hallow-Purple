d4b6060c85b1d030f07e1c56214fe1771ea7d872
Sharleyy <155279230+SH4RL1E@users.noreply.github.com>
2024-09-16 23:13:36 +0800

    Update js.js

--- js.js
+++ js.js
@@ -29,44 +29,6 @@ Timer.prototype.start = function() {
 	this.running = true;
 	var remainingSeconds = this.els.seconds.textContent = this.duration / 1000;
 	
-	function draw(now) {
-		if (!start) start = now;
-		var diff = now - start;
-		var newSeconds = Math.ceil((self.duration - diff)/1000);
-
-		if (diff <= self.duration) {
-			self.els.ticker.style.height = 100 - (diff/self.duration*100) + '%';
-			
-			if (newSeconds != remainingSeconds) {
-				self.els.seconds.textContent = newSeconds;
-				remainingSeconds = newSeconds;
-			}
-			
-			self.frameReq = window.requestAnimationFrame(draw);
-		} else {
-			//self.running = false;
-			self.els.seconds.textContent = 0;
-			self.els.ticker.style.height = '0%';
-			self.element.classList.add('countdown--ended');
-			window.location.href = 'crash.html';
-		}
-	};
-	
-	self.frameReq = window.requestAnimationFrame(draw);
 }
 
-Timer.prototype.reset = function() {
-	this.running = false;
-	window.cancelAnimationFrame(this.frameReq);
-	this.els.seconds.textContent = this.duration / 1000;
-	this.els.ticker.style.height = null;
-	this.element.classList.remove('countdown--ended');
-}
-
-Timer.prototype.setDuration = function(duration) {
-	this.duration = duration;
-	this.els.seconds.textContent = this.duration / 1000;
-}
-
-var timer = new Timer(10000, document.getElementById('countdown'));
-timer.start();
+/*tulog sa ko yw 
